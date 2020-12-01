4b07862877bf1d63fab21ba8e6da46bdf4141d4b
Aleksei Smyshliaev <smishlayev@gmail.com>
2020-12-01 18:14:25 +0000

    add Day 1 part two

--- aoc01.py
+++ aoc01.py
@@ -1,19 +1,45 @@
+from functools import reduce
 import sys
 
 
-def find_sum_2020(numbers):
-    ''' Return two numbers that add up to 2020 '''
+def find_sum_to(numbers, amount=1, total=0):
+    '''
+    Return an amount of numbers from the given list that add up to the desired total
+
+    :param numbers: list of numbers to select from
+    :param amount: how many numbers to choose
+    :param total: what is the desired sum
+    :returns: list with all individual numbers that add up to total
+    '''
+    if not amount:
+        return []
+
     for ii in range(len(numbers)):
-        for jj in range(ii + 1, len(numbers)):
-            if numbers[ii] + numbers[jj] == 2020:
-                return numbers[ii], numbers[jj]
-    raise ValueError('given list doesn\'t contain a pair of integers adding up to 2020')
+        result = []
+        num = numbers[ii]
+        remainder = total - num
+        if amount == 1:
+            if remainder == 0:
+                return [num]
+        else:
+            others = find_sum_to(numbers[ii + 1:], amount - 1, remainder)
+            if others:
+                return [num] + others
+    return []
 
 
 if __name__ == '__main__':
     numbers = []
     for line in sys.stdin:
         numbers.append(int(line))
-        
-    aa, bb = find_sum_2020(numbers)
-    print(aa * bb)
+
+    AMOUNT = 3
+    TOTAL = 2020
+
+    result = find_sum_to(numbers, AMOUNT, TOTAL)
+    
+    if not result:
+        raise ValueError(f'could not find {AMOUNT} numbers that add up to {TOTAL}')
+
+    print(result)
+    print(reduce(lambda a, b: a*b, result, 1))
