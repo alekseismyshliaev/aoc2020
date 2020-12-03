fdf1df45c908b16dc3f5c615a970c70d8a110c63
Aleksei Smyshliaev <smishlayev@gmail.com>
2020-12-03 18:12:56 +0000

    add Day 3 part two

--- aoc03.py
+++ aoc03.py
@@ -7,8 +7,11 @@ FREE_SPACE = '.'
 DX = 3
 DY = 1
 
-def count_trees(map_data, dx, dy):
+def count_trees(map_data, dx, dy, startx=0, starty=0):
     '''Count how many trees you encounter while traversing at the given slope
+
+    Traversing is done by jumping the given number of spaces down and to
+    the right, counting every tree that is encountered when landing in a space.
 
     :param map_data: list of strings, describing one column of the map.
         the map repeats horizontally.
@@ -16,8 +19,8 @@ def count_trees(map_data, dx, dy):
     :param dy: slope of the travel, vertical (integer steps)
     :returns: number of the tree spaces encountered on this slope
     '''
-    xx = 0
-    yy = 0
+    xx = startx
+    yy = starty
     tree_count = 0
 
     while yy < len(map_data):
@@ -33,6 +36,10 @@ if __name__ == '__main__':
     for line in sys.stdin:
         travel_map.append(line.rstrip('\n'))
 
-    tree_count = count_trees(travel_map, 3, 1)
+    tree_count = count_trees(travel_map, 1, 1)
+    tree_count *= count_trees(travel_map, 3, 1)
+    tree_count *= count_trees(travel_map, 5, 1)
+    tree_count *= count_trees(travel_map, 7, 1)
+    tree_count *= count_trees(travel_map, 1, 2)
 
     print(tree_count)
