ecdaa8c27fb45470e9e3fa471bcc1bfbab7537ab
Aleksei Smyshliaev <smishlayev@gmail.com>
2020-12-06 15:32:32 +0000

    add Day 6 part two

--- aoc06.py
+++ aoc06.py
@@ -4,5 +4,11 @@ import sys
 if __name__ == '__main__':
     groups = sys.stdin.read().strip().split('\n\n')
 
-    groups = [''.join(set(group.replace('\n', ''))) for group in groups]
-    print(sum(len(group) for group in groups))
+    groups = [group.split('\n') for group in groups]
+    all_answers = []
+    for group in groups:
+        answers = set(group[0])
+        for other in group[1:]:
+            answers = answers.intersection(set(other))
+        all_answers.append(answers)
+    print(sum(len(answers) for answers in all_answers))
